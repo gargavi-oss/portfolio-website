770dc4394276f5de8a743dff6f23eebc28302a5c
avi garg <gargavi2006@outlook.com>
2025-07-22 22:42:14 +0530

    added skills of backend

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-   base: process.env.VITE_BASE_PATH || "/portfolio-website",
+  plugins: [react(),tailwindcss()],  base: process.env.VITE_BASE_PATH ,
+ 
  
 })
